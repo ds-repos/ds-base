94abc078fb4d7c059d2161bc6fd94ff5fe76d1e8
pkgdemon <pkgdemonteam@gmail.com>
2024-01-25 01:59:13 -0500

    Be more robust about cleaning only things that exist

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -6,29 +6,55 @@ if [ "$(id -u)" -ne 0 ]; then
     exit 1
 fi
 
-# Source build.conf
-. ../conf/build.conf
+# Check if $SRC is defined
+if [ -z "$SRC" ]; then
+    echo "Error: \$SRC variable is not defined. Please set it in build.conf."
+    exit 1
+fi
 
 # Check if the directory specified by $SRC exists
 if [ ! -d "${SRC}" ]; then
+    echo "Error: Directory specified by \$SRC ('$SRC') does not exist."
     exit 0
 fi
 
+# Source build.conf
+. ../conf/build.conf
+
 libobjc2()
 {
-  cd ${SRC}/libobjc2 && cd Build && \
-    ninja clean
+  # Check if the directory exists before running ninja clean
+  if [ -d "${SRC}/libobjc2/Build" ]; then
+    cd ${SRC}/libobjc2 && cd Build && ninja clean
+  fi
 }
 
 gnustep()
 {
   # tools-make does not need cleaning so we skip and do others
   # We must reinstall tools-make to be able to clean items from here
-  cd ${SRC}/tools-make && ./configure && gmake && gmake install
+
+  # Check if the directory exists before running configure and make
+  if [ -d "${SRC}/tools-make" ]; then
+    cd ${SRC}/tools-make && ./configure && gmake && gmake install
+  fi
+
   . /usr/local/share/GNUstep/Makefiles/GNUstep.sh
-  cd ${SRC}/libs-base && gmake clean
-  cd ${SRC}/libs-gui && gmake clean
-  cd ${SRC}/libs-back && gmake clean
+
+  # Check if the directory exists before running gmake clean
+  if [ -d "${SRC}/libs-base" ]; then
+    cd ${SRC}/libs-base && gmake clean
+  fi
+
+  # Check if the directory exists before running gmake clean
+  if [ -d "${SRC}/libs-gui" ]; then
+    cd ${SRC}/libs-gui && gmake clean
+  fi
+
+  # Check if the directory exists before running gmake clean
+  if [ -d "${SRC}/libs-back" ]; then
+    cd ${SRC}/libs-back && gmake clean
+  fi
 }
 
 libobjc2
