4498309d120ef6c00a94202e8c3ba5b15468f2ee
pkgdemon <pkgdemonteam@gmail.com>
2024-01-24 23:50:36 -0500

    Add ninja build for libojbc2

--- scripts/build.sh
+++ scripts/build.sh
@@ -11,8 +11,8 @@ fi
 
 build()
 {
-  cd ${SRC}${libobjc2}
-  pwd
+  cd ${SRC}/libobjc2 && && mkdir Build && cd Build && \
+    cmake .. -G Ninja -DCMAKE_BUILD_TYPE=Debug -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++
 }
 
-#libobjc2
+build
