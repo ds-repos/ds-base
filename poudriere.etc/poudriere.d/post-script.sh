95dd229baafa03aa9d2c20bd058d1d18d243fe32
pkgdemon <pkgdemonteam@gmail.com>
2024-02-06 22:28:56 -0500

    Fixing adding user to multiple groups

--- poudriere.etc/poudriere.d/post-script.sh
+++ poudriere.etc/poudriere.d/post-script.sh
@@ -20,7 +20,7 @@ mkdir -p /Users/hexley/Documents
 mkdir -p /Users/hexley/Downloads
 pw useradd hexley -u 1000 \
   -c "Hexley" -d "/Users/hexley" \
-  -g staff,wheel -m -s /usr/local/bin/zsh -k /usr/share/skel -w none
+  -g staff wheel -m -s /usr/local/bin/zsh -k /usr/share/skel -w none
 chown -R hexley:staff /Users/hexley
 
 # Fetch the bsdstep zip from GitHub
