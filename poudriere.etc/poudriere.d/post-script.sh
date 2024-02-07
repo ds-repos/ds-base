80c981f61871b727aa1b9ce1fe757fcac852f399
pkgdemon <pkgdemonteam@gmail.com>
2024-02-06 23:45:37 -0500

    Again try to fix additional group

--- poudriere.etc/poudriere.d/post-script.sh
+++ poudriere.etc/poudriere.d/post-script.sh
@@ -17,7 +17,7 @@ mkdir -p /Users/hexley/Documents
 mkdir -p /Users/hexley/Downloads
 pw useradd hexley -u 1000 \
   -c "Hexley" -d "/Users/hexley" \
-  -g staff wheel -m -s /usr/local/bin/zsh -k /usr/share/skel -w none
+  -g staff -G wheel -m -s /usr/local/bin/zsh -k /usr/share/skel -w none
 chown -R hexley:staff /Users/hexley
 
 # Fetch the bsdstep zip from GitHub
